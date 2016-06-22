4ec929edfc67e55bf15367f1e618a69685388cbf
hangloos <ericloos00@gmail.com>
2016-06-22 14:35:43 -0400

    working flow in my application controller between index, login, signup and users home page. Links to signup and login on home page. Using helper  method logged_in? to see if logged in.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,41 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  get '/signup' do
+    if !logged_in?
+      erb :signup
+    else
+      redirect '/users/:id/home'
+    end
+  end
+
+  get '/login' do
+    if !logged_in?
+      erb :login
+    else
+      redirect '/users/:id/home'
+    end
+  end
+
+
+  get '/users/:id/home' do
+    if logged_in?
+      erb :'/users/home'
+    else
+      redirect '/login'
+    end
+  end
+
+
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+      User.find_by_id(session[:user_id])
+    end
+  end
+
 end
