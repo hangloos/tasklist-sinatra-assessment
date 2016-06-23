7979ac93be170cfefbba41454bd341bf0f59b553
hangloos <ericloos00@gmail.com>
2016-06-23 11:49:46 -0400

    added a tasks home page inside of the list you select via hyperlink. This page has a create task text box and a delete checkbox

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,11 +40,35 @@ class UsersController < ApplicationController
     redirect '/lists'
   end
 
+  get '/users/:slug' do
+  if logged_in? && current_user_logged_in?
+      @user = current_user
+      @list = List.find_by_slug(params[:slug])
+      erb :'/tasks/home'
+    else
+      redirect '/login'
+    end 
+  end
+
+  post '/users/tasks' do 
+    @list = List.find_by_id(params[:list][:id])
+    @task = Task.create(name: params[:task][:name])
+    @list.tasks << @task
+    redirect "/users/#{@list.slug}"
+  end
+
+  post '/users/tasks/delete' do
+    @list = List.find_by_id(params[:list][:id])
+    @task = Task.find_by_id(params[:task][:id])
+    @task.delete
+    redirect "/users/#{@list.slug}"
+  end
+
 
 
 
 end
 
-#eric
-#harper
-# list - 
+# testing info:
+#user1
+#1
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,13 @@ class List < ActiveRecord::Base
   has_many :list_tasks
   has_many :tasks, through: :list_tasks
   has_many :task_types
-  has_many :types, through: :tasks 
+  has_many :types, through: :tasks
+
+  def slug
+    self.name.split(" ").join("-").downcase
+  end
+
+  def self.find_by_slug(word)
+    List.all.find{|list| list.slug == word}
+  end 
 end
